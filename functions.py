3a6d1be84c177a3509a6e141fc7705faee7130b9
SergSemak <semakin.s.v@gmail.com>
2023-07-24 01:24:03 +0300

    added func edit_note

--- functions.py
+++ functions.py
@@ -23,10 +23,10 @@ def main():
   while(key_input != 0):
       if key_input == 1:
         show_notes()
-      if key_input == 2:
+      elif key_input == 2:
         create_note_json()   
-      # elif key_input == 3:
-      #     add_contact(file_path)
+      elif key_input == 3:
+        edit_note(int(input(Fore.YELLOW + 'Введите номер заметки: ')))
       elif key_input == 4:
          read_note(int(input(Fore.YELLOW + 'Введите номер заметки: ')))
       elif key_input == 5:
@@ -36,6 +36,7 @@ def main():
       key_input = int(input("Введите цифру: "))
   print(Fore.GREEN + 'До свидания!\n')
 
+# Просмотр всех заметок
 def show_notes():
    id = 1
   #  path_file = Path('notes', f'{id}.json')
@@ -46,21 +47,25 @@ def show_notes():
 
 # Создание новой заметки
 def create_note_json():
-  file_name = getId()
-  file_title = input(Fore.YELLOW + 'Введите заголовок: ' + Fore.RESET)
-  file_text = input(Fore.YELLOW + 'Текст заметки: ' + Fore.RESET)
+  id = getId()
+  title = input(Fore.YELLOW + 'Введите заголовок: ' + Fore.RESET)
+  text = input(Fore.YELLOW + 'Текст заметки: ' + Fore.RESET)
   time_create = datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S)")
-  path_file = str(Path('notes', f'{file_name}.json'))
-  new_file = open(path_file, "w")
-  new_file.write("{\n")
-  new_file.write(f"\t\"id\": {file_name},\n")
-  new_file.write(f"\t\"title\": \"{file_title}\",\n")
-  new_file.write(f"\t\"text\": \"{file_text}\",\n")
-  new_file.write(f"\t\"time_create\": \"{time_create}\",\n")
-  new_file.write("\t\"time_change\": \"-\"\n}")
-  new_file.close
-  # notes[file_name] = path_file
+  path_file = str(Path('notes', f'{id}.json'))
+  save_to_json(path_file, id, title, text, time_create, '-')
   print(Fore.GREEN + 'Вы создали новую заметку\n' + Fore.RESET)
+
+# Редактирование заметки
+def edit_note(id):
+    print('Старый вариант:')
+    read_note(id)
+    path_file = str(Path('notes', f'{id}.json'))
+    note_dic = load_from_json(path_file)
+    title = input(Fore.YELLOW + 'Новый заголовок: ' + Fore.RESET)
+    text = input(Fore.YELLOW + 'Новый текст заметки: ' + Fore.RESET)
+    time_change = datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S)")
+    save_to_json(path_file, note_dic['id'], title, text, note_dic['time_create'], time_change)
+    print(Fore.GREEN + 'Заметка изменена\n' + Fore.RESET)
 
 # Чтение заметки по его id
 def read_note(id):
@@ -103,6 +108,16 @@ def getId():
       id = id + 1
    return id
 
+def save_to_json(path_file, file_name, file_title, file_text, time_create, time_change):
+  new_file = open(path_file, "w")
+  new_file.write("{\n")
+  new_file.write(f"\t\"id\": {file_name},\n")
+  new_file.write(f"\t\"title\": \"{file_title}\",\n")
+  new_file.write(f"\t\"text\": \"{file_text}\",\n")
+  new_file.write(f"\t\"time_create\": \"{time_create}\",\n")
+  new_file.write(f"\t\"time_change\": \"{time_change}\"\n")
+  new_file.write("}\n")
+  new_file.close()
 # def json_to_string(file_path):
 #   json = "" 
 #   with open(file_path, 'r') as note:
